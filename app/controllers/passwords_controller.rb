6fee3bc3764b21e21056a2eeb3efcbab04492775
William <rafael@webgoal.com.br>
2016-08-28 12:01:33 -0300

    Metodos da crontroller create index

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,12 +1,27 @@
 class PasswordsController < ApplicationController
 	def new
+		@password = Passwords.new
 
 	end
 
 	def create
+    @password = Passwords.new(password_param)
 
-	end
+    respond_to do |format|
+      if @password.save
+        format.html { redirect_to @password, notice: 'Senha cadastrada com sucesso.' }
+        format.json { render :show, status: :created, location: @password }
+      else
+        format.html { render :new }
+        format.json { render json: @password.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 	def index
   end
+
+	def password_params
+		params.require(:password).permit(:user, :description, :password, :password_confirm, :url)
+	end
 end
